18a778c0af47b3184eeb6b643639bd28373d388f
Doeun Kwon <dekwooon@gmail.com>
2024-11-28 10:32:20 -0800

    add both prod and dev client url to CORS

--- server/app/main.py
+++ server/app/main.py
@@ -9,12 +9,15 @@ app = FastAPI(
 
 # Get environment variables with defaults
 PORT = int(os.getenv("PORT", 8000))
-CLIENT_URL = os.getenv("CLIENT_URL", "http://localhost:3000")
+ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://carina-jx69.onrender.com"
+]
 
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[CLIENT_URL],  # You can add multiple origins in this list
+    allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
