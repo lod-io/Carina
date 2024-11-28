8ded3a50698c1e4fbf0a4e007ddd07345b6cf030
Doeun Kwon <dekwooon@gmail.com>
2024-11-28 10:15:12 -0800

    dynamic client url for server

--- server/app/main.py
+++ server/app/main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
 from app.api.routes import api_router
 
 app = FastAPI(
     title="AI Architect"
 )
 
+# Get environment variables with defaults
+PORT = int(os.getenv("PORT", 8000))
+CLIENT_URL = os.getenv("CLIENT_URL", "http://localhost:3000")
+
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React frontend
+    allow_origins=[CLIENT_URL],  # You can add multiple origins in this list
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
